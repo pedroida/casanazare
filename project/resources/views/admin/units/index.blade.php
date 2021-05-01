f1c15d5958af6149cc38711af11967a9fd7c1ff9
pedroida <pedrohsida@hotmail.com>
2021-05-01 14:37:46 -0300

    fixing heading

--- project/resources/views/admin/units/index.blade.php
+++ project/resources/views/admin/units/index.blade.php
@@ -4,7 +4,7 @@
 @section('page-header')
     <h1>
         <i class="fas fa-building fa-fw mr-2 text-muted"></i>
-        @lang('headings.units111111.index')
+        @lang('headings.units.index')
     </h1>
 
     <breadcrumb>
